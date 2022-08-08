cbfa8bd26ee346b6b630631e5b58e2d299fb2e3d
wukaiwei1 <2596862515@qq.com>
2022-08-08 09:11:34 +0800

    合并设备管理页面功能分支

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,6 @@
 export * from './user.js'
 export * from './task.js'
-<<<<<<< HEAD
 export * from './equipment'
-=======
-
 export * from './people.js'
-
 export * from './operation.js'
->>>>>>> e68c74fcd2af58bec1b66f71813ced6129ed5b42
+
